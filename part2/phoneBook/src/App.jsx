0906aafe0ece6c0087e5c8dddf588f7efc6ae407
martinranzuglia <martinranzuglia@gmail.com>
2025-12-15 13:51:54 -0300

    Ejercicio 2.9 resuelto

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState( [ { name: 'Arto Hellas', number: '341-585-1661' } ] );
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [ newNumber, setNewNumber ] = useState(''); 
   const [ newName, setNewName ] = useState('')
+  const [ filterValue, setFilterValue ] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -12,6 +18,14 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   }
+
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value)
+  }
+
+  const filteredPersons = (filterValue === "")
+    ?persons
+    :persons.filter( person => person.name.toLowerCase().includes(filterValue.toLowerCase()) );
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -29,6 +43,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>Filter shown with <input onChange={ handleFilterChange } value={filterValue} /></div>
+      </form>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleNameChange} value={newName} placeholder='Introduce un nombre...' />
@@ -42,7 +59,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name} >{person.name} {person.number} </li>)}
+        {filteredPersons.map(person => <li key={person.name} >{person.name} {person.number} </li>)}
       </ul>
     </div>
   )
